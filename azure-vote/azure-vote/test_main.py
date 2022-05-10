240b269ec4715af06f055c1f57bf1d844b491808
demo202204 <demo@nswmpc.microsoft.com>
2022-05-10 06:17:06 +0000

    CI新規登録

---
+++ azure-vote/azure-vote/test_main.py
@@ -0,0 +1,32 @@
+"""Test Code"""
+import pytest
+
+from main import app
+
+
+@pytest.fixture()
+def client(request):
+    app.config['TESTING'] = True
+    test_client = app.test_client()
+    yield test_client
+    test_client.delete()
+
+
+def vote(client, vote):
+    return client.post('/', data=dict(vote=vote), follow_redirects=True)
+
+
+def test_get(client):
+    test_client = client
+    result = test_client.get('/')
+    """
+    テスト処理実施
+    """
+    # assert テスト評価
+    assert result.status_code == 200
+    assert 'Azure Voting App' in result.get_data(as_text=True)
+
+
+def test_Fail(client):
+    # テスト失敗時
+    assert False
